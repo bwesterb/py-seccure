5609d222910ba8f39c42b13e0465b3f7eb111fc1
Bas Westerbaan <bas@westerbaan.name>
2013-06-26 13:42:56 +0200

    Add tests for compact format

--- src/tests/test_helpers.py
+++ src/tests/test_helpers.py
@@ -14,6 +14,8 @@ class TestHelpers(unittest.TestCase):
                     (256,           b'0100')):
             self.assertEqual(binascii.hexlify(seccure.serialize_number(number)),
                                         string)
+            self.assertEqual(seccure.deserialize_number(
+                            binascii.unhexlify(string)), number)
         for number, string in (
                     (1231234,       b'0012c982'),
                     (0,             b'00000000'),
@@ -23,6 +25,36 @@ class TestHelpers(unittest.TestCase):
             self.assertEqual(binascii.hexlify(
                     seccure.serialize_number(number, outlen=4)),
                                         string)
+            self.assertEqual(seccure.deserialize_number(
+                            binascii.unhexlify(string)), number)
+    def test_serialize_number_compact(self):
+        for number, string in (
+                    (1231234,       b'#c!E'),
+                    (0,             b''),
+                    (255,           b'$p'),
+                    (1231231231232, b'$?Pvfdc'),
+                    (13371337,      b'5AkH'),
+                    (90,            b'#!'),
+                    (89,            b'~'),
+                    (256,           b'$q')):
+            self.assertEqual(seccure.serialize_number(number,
+                        fmt=seccure.SER_COMPACT), string)
+            self.assertEqual(seccure.deserialize_number(
+                            string, fmt=seccure.SER_COMPACT), number)
+        for number, string in (
+                    (1231234,       b'!!!!!!#c!E'),
+                    (0,             b'!!!!!!!!!!'),
+                    (255,           b'!!!!!!!!$p'),
+                    (1231231231232, b'!!!$?Pvfdc'),
+                    (13371337,      b'!!!!!!5AkH'),
+                    (90,            b'!!!!!!!!#!'),
+                    (89,            b'!!!!!!!!!~'),
+                    (256,           b'!!!!!!!!$q')):
+            self.assertEqual(
+                    seccure.serialize_number(number, outlen=10,
+                            fmt=seccure.SER_COMPACT), string)
+            self.assertEqual(seccure.deserialize_number(string,
+                            fmt=seccure.SER_COMPACT), number)
 
 
 if __name__ == '__main__':
